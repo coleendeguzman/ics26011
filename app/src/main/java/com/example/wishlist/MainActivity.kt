f842b68c1dbf3f44e315d533f34203480df47503
Coleen de Guzman <coleen.deguzman.cics@ust.edu.ph>
2023-11-18 01:48:19 +0800

    Add Item Page

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.wishlist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
