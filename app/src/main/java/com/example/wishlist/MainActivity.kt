69ad917f1c58e042cf33e8060ffd1edcb81ae178
Coleen de Guzman <coleen.deguzman.cics@ust.edu.ph>
2023-11-19 16:11:06 +0800

    Add dialog box

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.wishlist
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
