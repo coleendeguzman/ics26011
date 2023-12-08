a67dcfb84fb90c99fc61d53dde56c638882d34bf
Coleen <coleen.deguzman.cics@ust.edu.ph>
2023-12-08 16:11:08 +0800

    update dialog box updated.. may clear button rin!

--- app/src/main/java/com/example/wishlist/UpdateWish.kt
+++ app/src/main/java/com/example/wishlist/UpdateWish.kt
@@ -1,19 +1,28 @@
 package com.example.wishlist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.Window
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.RadioGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wishlist.databinding.UpdateWishPageBinding
 
 class UpdateWish : AppCompatActivity() {
 
+    private lateinit var binding: UpdateWishPageBinding
     private lateinit var wish: Wishes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.update_wish_page)
+        binding = UpdateWishPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val wishId = intent.getIntExtra("wish_id", -1) // Get wish ID from intent
 
@@ -21,10 +30,55 @@ class UpdateWish : AppCompatActivity() {
 
         populateFields()
 
-        val updateButton = findViewById<ImageView>(R.id.update)
-        updateButton.setOnClickListener {
-            updateWishDetails()
+        val btnUpdate = binding.update
+        val btnBack = binding.back
+        val btnClear = binding.clear
+
+
+        btnUpdate.setOnClickListener {
+            showUpdateConfirmationDialog()
         }
+
+        btnBack.setOnClickListener {
+            onBackPressed()
+        }
+
+        btnClear.setOnClickListener {
+            clearFields()
+        }
+    }
+
+    private fun clearFields() {
+        binding.updateName.text.clear()
+        binding.updateLink.text.clear()
+        binding.updateDesc.text.clear()
+        // Clear the selected radio button in the RadioGroup if needed
+        binding.rgUpdateCategory.clearCheck()
+    }
+
+    override fun onBackPressed() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.cancel_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.setOnKeyListener { _, keyCode, _ ->
+            keyCode == KeyEvent.KEYCODE_BACK
+        }
+
+        val btnCancel = dialog.findViewById<Button>(R.id.btnNo)
+        val btnConfirm = dialog.findViewById<Button>(R.id.btnYes)
+
+        btnCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        btnConfirm.setOnClickListener {
+            dialog.dismiss()
+            super.onBackPressed() // Go back only if confirmed
+        }
+
+        dialog.show()
     }
 
     private fun fetchWishDetails(wishId: Int) {
@@ -33,10 +87,10 @@ class UpdateWish : AppCompatActivity() {
     }
 
     private fun populateFields() {
-        val wishNameEditText = findViewById<EditText>(R.id.updateName)
-        val wishLinkEditText = findViewById<EditText>(R.id.updateLink)
-        val wishDescEditText = findViewById<EditText>(R.id.updateDesc)
-        val radioGroupCategory = findViewById<RadioGroup>(R.id.rgUpdateCategory)
+        val wishNameEditText = binding.updateName
+        val wishLinkEditText = binding.updateLink
+        val wishDescEditText = binding.updateDesc
+        val radioGroupCategory = binding.rgUpdateCategory
 
         wishNameEditText.setText(wish.wishname)
         wishLinkEditText.setText(wish.wishlink)
@@ -50,13 +104,12 @@ class UpdateWish : AppCompatActivity() {
         }
     }
 
-    private fun updateWishDetails() {
-        val databaseHandler = DatabaseHandler(this)
-        val updatedWishName = findViewById<EditText>(R.id.updateName).text.toString()
-        val updatedWishLink = findViewById<EditText>(R.id.updateLink).text.toString()
-        val updatedWishDesc = findViewById<EditText>(R.id.updateDesc).text.toString()
+    private fun showUpdateConfirmationDialog() {
+        val updatedWishName = binding.updateName.text.toString()
+        val updatedWishLink = binding.updateLink.text.toString()
+        val updatedWishDesc = binding.updateDesc.text.toString()
 
-        val radioGroupCategory = findViewById<RadioGroup>(R.id.rgUpdateCategory)
+        val radioGroupCategory = binding.rgUpdateCategory
         val selectedCategoryRadioButtonId = radioGroupCategory.checkedRadioButtonId
         val selectedCategory = when (selectedCategoryRadioButtonId) {
             R.id.rb_school -> "SCHOOL"
@@ -66,6 +119,33 @@ class UpdateWish : AppCompatActivity() {
             else -> "DefaultCategory" // Set a default category if none is selected
         }
 
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.update_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btnYes = dialog.findViewById<Button>(R.id.btnYes)
+        val btnNo = dialog.findViewById<Button>(R.id.btnNo)
+
+        btnYes.setOnClickListener {
+            dialog.dismiss()
+            updateWish(updatedWishName, updatedWishLink, updatedWishDesc, selectedCategory)
+        }
+
+        btnNo.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    private fun updateWish(
+        updatedWishName: String,
+        updatedWishLink: String,
+        updatedWishDesc: String,
+        selectedCategory: String
+    ) {
+        val databaseHandler = DatabaseHandler(this)
         wish = Wishes(wish.id, updatedWishName, updatedWishLink, updatedWishDesc, selectedCategory)
         databaseHandler.updateWish(wish.id, updatedWishName, updatedWishLink, updatedWishDesc, selectedCategory)
 
@@ -73,4 +153,3 @@ class UpdateWish : AppCompatActivity() {
         finish()
     }
 }
-
